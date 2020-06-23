f35593ccf1ede373cc9b7ed1d308bed97bd7c4ff
Micah Bowie <micahbowie20@gmail.com>
2020-06-23 09:40:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
 
 def countdown
-  number = 5 
+  number = 10 
   
-  while number > 5
+  while number > 10
    puts "#{number} SECOND(S)!"
    number -=1 
-   sleep 1 
   end 
   return "HAPPY NEW YEAR!"
 end
