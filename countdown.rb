e38473461f7414461bd5dfd91209ece2fdeec9d7
Micah Bowie <micahbowie20@gmail.com>
2020-06-23 09:51:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,22 @@
-def countdown_with_sleep 
-  delay 5.0;
-end 
 
-def countdown
-  number = 10 
-  
-  while number > 10
-   puts "#{number} SECOND(S)!"
-   number -=1 
-  end 
-  return "HAPPY NEW YEAR!"
+
+
+
+def countdown(n)
+  while n > 0 
+  puts "#{n} SECOND(S)!"
+  n -=1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  sleep(5)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -=1
+  end
+  "HAPPY NEW YEAR!"
+end
+  
